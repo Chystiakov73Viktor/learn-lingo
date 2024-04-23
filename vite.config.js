0d742cd065a752cbb106ca2ede1e6b152ac182b0
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-04-23 12:34:31 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      src: '/src',
-      components: '/src/components',
-      pages: '/src/pages',
-      constants: '/src/constants',
-      images: '/src/images',
+      src: './src',
+      components: './src/components',
+      pages: './src/pages',
+      constants: './src/constants',
+      images: './src/images',
     },
   },
-})
+});
