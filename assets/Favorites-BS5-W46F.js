095f2cba2d1762b8e4198d069df5c7a929ad6679
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-05-06 06:35:06 +0000

    Deploying to gh-pages from @ Chystiakov73Viktor/learn-lingo@1e9221d472173af0eccb1bdea221930fab1bed36 🚀

---
+++ assets/Favorites-BS5-W46F.js
@@ -0,0 +1,20 @@
+import{u as n,j as e,c as t,L as d}from"./index-BPHDKoSP.js";import{S as l}from"./Section-D7uq9Ja5.js";import{b as c,s as g,F as m,P as h,C as u}from"./CardMarkupList-DnR3pBhd.js";const x="/learn-lingo/assets/default-CR71aU9P.jpg",f=n.div`
+  .containerImg {
+    width: 100%;
+    height: 700px;
+    overflow: hidden;
+  }
+
+  .defaultImg {
+    width: 100%;
+    height: 100%;
+    border-radius: 14px;
+    background: #f3f3f2,
+      linear-gradient(
+        180deg,
+        rgba(18, 20, 23, 0.5) 0%,
+        rgba(18, 20, 23, 0.5) 100%
+      );
+    object-fit: cover;
+  }
+`,j=()=>e.jsx(f,{children:e.jsx("div",{className:"containerImg ",children:e.jsx("img",{className:"defaultImg",src:x,alt:"placeholder"})})});function C(){const a=t(c),s=t(g),r=4,i=o=>e.jsx(u,{dataCard:o});return e.jsx(l,{className:"carsListSection",titleClassName:"titleCatalog",children:s?e.jsx(d,{}):e.jsx(e.Fragment,{children:a.length!==0?e.jsxs(e.Fragment,{children:[e.jsx(m,{}),e.jsx(h,{data:a,itemsPerPage:r,renderCardList:i})]}):e.jsx(j,{})})})}export{C as default};
