a17e7f5cd237a591f39d5e446d2160f76a46f1e8
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-05-11 16:43:25 +0000

    Deploying to gh-pages from @ Chystiakov73Viktor/learn-lingo@10e26c6002ed927b20710abe86ea54910edb56ac 🚀

--- assets/Section-ClH-qtfU.js
+++ assets/Section-ClH-qtfU.js
@@ -1,4 +1,4 @@
-import{u as a,j as n,C as i}from"./index-BPX7pj3w.js";const t=a.section`
+import{u as a,j as n,C as i}from"./index-Buc1UnlI.js";const t=a.section`
   padding: 80px 0;
   background: #f8f8f8;
   &.hero {
