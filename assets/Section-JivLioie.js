3cc679db9c207ac1a45459195a5b3d940b0890a8
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-05-19 09:12:30 +0000

    Deploying to gh-pages from @ Chystiakov73Viktor/learn-lingo@1ed75f51ff76c3bbb8261a50239a20d9ec9f1b2d 🚀

--- assets/Section-JivLioie.js
+++ assets/Section-JivLioie.js
@@ -1,4 +1,4 @@
-import{u as a,j as n,C as i}from"./index-Dd7AxfCr.js";const t=a.section`
+import{u as a,j as n,C as i}from"./index-DW4-whdy.js";const t=a.section`
   padding: 80px 0;
   background: #f8f8f8;
   &.hero {
